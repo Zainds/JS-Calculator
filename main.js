1bc92a04b4c472c7c942757fe94520c581dc46ae
Zainds <beat1097@mail.ru>
2025-03-28 14:01:19 +0700

    Create calculator template

---
+++ main.js
@@ -0,0 +1,31 @@
+let number;
+let operation;
+let numberAnother;
+
+function add(a, b) {
+  return a + b;
+}
+function substract(a, b) {
+  return a - b;
+}
+function multiply(a, b) {
+  return a * b;
+}
+function divide(a, b) {
+  return a / b;
+}
+function operate(a, operation, b) {
+  switch (operation) {
+    case "+":
+      return add(a, b);
+
+    case "-":
+      return substract(a, b);
+
+    case "*":
+      return multiply(a, b);
+
+    case "/":
+      return divide(a, b);
+  }
+}
